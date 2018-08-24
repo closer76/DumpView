20ecb5310d1d1b8567a06ed35b0e4999804b6ba5
Kenneth Lo <closer.tw@gmail.com>
2018-08-24 17:11:33 +0800

    (Official release)

--- DumpView/version.h
+++ DumpView/version.h
@@ -6,7 +6,7 @@
 const wxString APP_NAME = wxT("DumpView");
 const wxString WINDOW_TITLE = wxT("Dump View");
 
-const wxString VER = wxT("1.10");
+const wxString VER = wxT("1.11");
 const wxString BUILD_DATE = wxT(__DATE__);
 const wxString BUILD_TIME = wxT(__TIME__);
 
